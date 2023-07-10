89aecc894c8c52556da7bb7dd29c59c847869eaa
wanghanwen <wanghanwen@lxslc706.ihep.ac.cn>
2023-07-10 18:37:53 +0800

    add script for automatic job submitting

--- script/prepare_all_jobs.py
+++ script/prepare_all_jobs.py
@@ -1,4 +1,8 @@
 import os, sys
+import subprocess
+import glob
+
+# Execute a Python script
 
 def list_txt_files(directory):
     txt_files = []
@@ -7,14 +11,61 @@ def list_txt_files(directory):
             txt_files.append(filename)
     return txt_files
 
-if len(sys.argv) < 2:
-    raise OSError("Usage: python script/prepare_all_jobs.py <output_dir>")
+if len(sys.argv) < 3:
+    raise OSError("Usage: python script/prepare_all_jobs.py <input_table> <output_dir> <run_type>")
 else:
-    output_tmp = sys.argv[1]    
+    input_tmp = sys.argv[1]
+    output_tmp = sys.argv[2]    
+if len(sys.argv) == 4:
+    runType = sys.argv[3]
+else:
+    runType = "main"
+table_path = os.path.abspath(input_tmp)
 output_dir = os.path.abspath(output_tmp)
+main_runs = []
+light_runs = []
+
+with open(table_path, 'r') as file:
+    for line in file:
+        line = line.strip()
+        if line:
+            key, value = line.split(':')
+            main_runs.append(int(key.strip()))
+            light_runs.append(int(value.strip()))
+
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
 output_dir = os.path.abspath(output_tmp)
-for aFile in list_txt_files("datasets"):
-    os.system(f"python3 script/prepare_skim_jobs.py datasets/{aFile} {output_dir}")
+
+os.system("sh get_datasets.sh")
+# Get the current directory
+current_directory = os.getcwd()
+
+# Find all "*.log" files in the current directory
+log_files = glob.glob(os.path.join(current_directory, '*.log'))
+
+# Iterate over the found files and remove them
+for file_path in log_files:
+    os.remove(file_path)
+# Remove elements that don't contain "main" or "light"
+filtered_list = [item for item in list_txt_files("datasets") if "main" in item or "light" in item]
+# Sort the list in ascending order
+sorted_list = sorted(filtered_list, key=lambda x: int(x.split('_')[2][:-4]))
+# Group the "light_run" elements together while preserving the original order
+grouped_list = sorted(sorted_list, key=lambda x: ("light_run" not in x, sorted_list.index(x)))
+
+for aFile in grouped_list:
+    name_short = aFile.split("/")[-1].replace(".txt", "")
+    run = int(name_short.split("_")[-1])
+    run_type = name_short.split('_')[0]
+    if run_type != runType:
+        continue
+    if run_type == "main" and not (run in main_runs):
+        continue
+    if run_type == "light" and not (run in light_runs):
+        continue
+    if run_type != "main" and run_type != "light":
+        continue
+    #os.system(f"python3 script/prepare_skim_jobs.py datasets/{aFile} {output_dir} {input_table}")
+    subprocess.run(['python', 'script/prepare_skim_jobs.py',f'datasets/{aFile}', f'{output_dir}', f'{table_path}'])
 
--- script/prepare_skim_jobs.py
+++ script/prepare_skim_jobs.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import os, sys
 from copy import deepcopy
+import yaml
 if len(sys.argv) < 3:
-    print("Usage: python prepare_jobs <input_file> <output_dir>")
+    print("Usage: python prepare_jobs <input_file> <output_dir> <intput_table>")
 else:
    input_tmp = sys.argv[1]
    output_tmp = sys.argv[2]
@@ -10,7 +11,10 @@ else:
 file_list =  os.path.abspath(input_tmp)  # Path to the file containing the list of files
 eos_mgm_url = "root://junoeos01.ihep.ac.cn"
 directory = "/tmp/tao-sipmtest"
+input_file = os.path.abspath(input_tmp)
 output_dir = os.path.abspath(output_tmp)
+name_short = input_file.split("/")[-1].replace(".txt", "")
+output_dir += "/" + name_short
 #output_dir = "/junofs/users/wanghanwen/main_run_0075"
 
 script  = ''
@@ -82,10 +86,21 @@ for file_path in files:
         with open(f'{output_dir}/jobs/{output_name}.sh','w') as file_tmp:
             file_tmp.write(script_tmp)
        
-if count != 192:
-    print(f"WARNING, there are only {count} lines and it's less than 192")
+if "main" in name_short and count != 192:
+    print(f"{name_short} : WARNING, there are only {count} lines and it's less than 192")
+    with open("incompleteDataInfo.log", 'a') as file:
+        line_of_text = f"main run {run_number} {count} 192"
+        file.write(line_of_text + '\n')
+elif "light" in name_short and count != 64:
+    print(f"{name_short} : WARNING, there are only {count} lines and it's less than 64")
+    with open("incompleteDataInfo.log", 'a') as file:
+        line_of_text = f"light run {run_number} {count} 64"
+        file.write(line_of_text + '\n')
 else:
-    print(f"Success, there are {count} lines in the datasets (should be 192)")
+    print(f"{name_short} : Success, there are {count} lines in the datasets ({'64 for light' if name_short.split('_')[0] == 'light' else '192 for main'})")
+    with open("completeDataInfo.log", 'a') as file:
+        line_of_text = f"{name_short.split('_')[0]} run {run_number} {count} {64 if name_short.split('_')[0] == 'light' else 192}"
+        file.write(line_of_text + '\n')
 
 
 os.system(f"chmod +x {output_dir}/jobs/*.sh")
