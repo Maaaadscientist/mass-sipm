4d92dc69d6039a58b8b3f9542794dd9f0b1345d2
Allen <cobby319@163.com>
2023-07-13 23:21:16 +0800

    add script for extracting final results

---
+++ script/extract_all_pdfs.py
@@ -0,0 +1,71 @@
+import os, sys
+import subprocess
+import glob
+
+# Execute a Python script
+
+def list_txt_files(directory):
+    txt_files = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".txt"):
+            txt_files.append(filename)
+    return txt_files
+
+if len(sys.argv) < 3:
+    raise OSError("Usage: python script/prepare_all_jobs.py <table_path> <input_path> <output_path> ")
+else:
+    table_tmp = sys.argv[1]    
+    input_tmp = sys.argv[2]
+    output_tmp = sys.argv[3]    
+
+table_path = os.path.abspath(table_tmp)
+input_dir = os.path.abspath(input_tmp)
+output_dir = os.path.abspath(output_tmp)
+runType = "main"
+
+    
+if not os.path.isdir(f'{output_dir}'):
+    os.makedirs(f'{output_dir}')
+main_runs = []
+light_runs = []
+
+with open(table_path, 'r') as file:
+    for line in file:
+        line = line.strip()
+        if line:
+            key, value = line.split(':')
+            main_runs.append(int(key.strip()))
+            light_runs.append(int(value.strip()))
+
+if not os.path.isdir(output_dir):
+    os.makedirs(output_dir)
+output_dir = os.path.abspath(output_tmp)
+
+
+# Remove elements that don't contain "main" or "light"
+filtered_list = [item for item in list_txt_files("datasets") if "main" in item or "light" in item]
+# Sort the list in ascending order
+sorted_list = sorted(filtered_list, key=lambda x: int(x.split('_')[2][:-4]))
+# Group the "light_run" elements together while preserving the original order
+grouped_list = sorted(sorted_list, key=lambda x: ("light_run" not in x, sorted_list.index(x)))
+
+for aFile in grouped_list:
+    name_short = aFile.split("/")[-1].replace(".txt", "")
+    run = int(name_short.split("_")[-1])
+    run_type = name_short.split('_')[0]
+    if run_type != runType:
+        continue
+    if run_type == "main" and not (run in main_runs):
+        continue
+    if run_type == "light" and not (run in light_runs):
+        continue
+    if run_type != "main" and run_type != "light":
+        continue
+
+    analysis_type = "harvest"
+    command = f'cp  {input_dir}/{analysis_type}/{name_short}  {output_dir}'
+    #print(command)
+
+    # Execute the shell script
+    #process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
