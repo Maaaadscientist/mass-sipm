89067393a8cdd508b2bbec2f16632ca3554df47f
Allen <cobby319@163.com>
2023-08-20 21:51:02 +0800

    update geometry fitter

--- script/design-coordinates.py
+++ script/design-coordinates.py
@@ -78,6 +78,8 @@ def convert_coordinates_4x4(x_po, y_po, x, y, yaml_data, original = (0 , 0)):
     y_real = y + (y_po - 1) * tile_offset_y - original[1]
     return x_real, y_real
 
+def convert_1to4(x,y):
+    return [(2*x-1, 2*y-1), (2*x, 2*y-1), (2*x-1, 2*y), (2*x, 2*y)]
 
 def average_light_intensity_square(x_center, y_center, square_length, rbf):
     # Calculate the coordinates for the square's corners
@@ -158,7 +160,9 @@ if __name__ == '__main__':
         po_list = []
         pt_list = []
         z_list = []
+        z_good_list = []
         file1 = ROOT.TFile(f"{file}")
+        file2 = ROOT.TFile("/Users/wanghanwen/codes/new666/harvest/main_run_0162/maps_run162.root")
         hname = "light_map_full"
         hname2 = "reff_mu_1D"
         reff_x = []
@@ -168,13 +172,10 @@ if __name__ == '__main__':
         for po in range(16):
         
             light_hist = file1.Get(hname + f"/{hname}_tile{po}")
+            light_hist_good = file2.Get(hname + f"/{hname}_tile{po}")
             reff_hist = file1.Get(hname2 + f"/reff_mu_1D_tile{po}")
             x_po, y_po = get_coordinates_4x4(po + 1)
             x_ref, y_ref = convert_coordinates_4x4(x_po, y_po, float(ref_offset_x), float(ref_offset_y), yaml_data)
-            #x_list.append(x_ref)
-            #y_list.append(y_ref)
-            #po_list.append(po)
-            #pt_list.append(0)
             pde = reff_data.get(po)
             reff_light_intensity = reff_hist.GetMean()/pde
             reff_stddev = reff_hist.GetRMS()/pde
@@ -193,33 +194,11 @@ if __name__ == '__main__':
                 pt_list.append(point)
                 mu = light_hist.GetBinContent(x_pt, y_pt)
                 z_list.append(mu / pde)
-        square_length = 6  # Adjust the length of the square's side as needed
-        
-        plt.figure(figsize=(12, 8))  # Enlarged figure size
-        
-        #plt.scatter(x_list, y_list, color='blue', marker='o', s=100)
-        
-        for i, (po, pt) in enumerate(zip(po_list, pt_list)):
-            plt.text(x_list[i], y_list[i], f'({po}, {pt})', fontsize=3, ha='center', va='bottom')
-        
-            # Calculate the coordinates for the square's lower-left corner
-            square_x = x_list[i] - square_length / 2
-            square_y = y_list[i] - square_length / 2
-        
-            # Create a Rectangle patch and add it to the plot
-            square = Rectangle((square_x, square_y), square_length, square_length, edgecolor='red', fill=False)
-            plt.gca().add_patch(square)
-        
-        plt.xlabel('X (mm)')
-        plt.ylabel('Y (mm)')
-        plt.title('Reference SiPM positions')
-        
-        plt.axis('equal')
-        
-        plt.savefig(f"{filepath}/{name_without_extension}_positions.pdf")
-        plt.clf()
+                mu_good = light_hist_good.GetBinContent(x_pt, y_pt)
+                z_good_list.append(mu_good / pde)
         # Create the interpolation/extrapolation function
         rbf = Rbf(x_list, y_list, z_list, function='linear')
+        rbf_good = Rbf(x_list, y_list, z_good_list, function='linear')
         
         # Specify the coordinates where you want to interpolate/extrapolate
         x_new = np.linspace(-20, 300, 1280)  # Note that we are extending the range for extrapolation
@@ -264,18 +243,24 @@ if __name__ == '__main__':
         
         # Calculate the chi-squared statistic for each offset
         chi2_values = np.zeros_like(dx)
+        chi2_values_good = np.zeros_like(dx)
         for i in range(len(offset_range)):
             for j in range(len(offset_range)):
                 x_unknown = initial_x + dx[i, j]
                 y_unknown = initial_y + dy[i, j]
                 expected_intensities = rbf(x_unknown, y_unknown)
+                expected_intensities_good = rbf_good(x_unknown, y_unknown)
                 chi2_values[i, j] = np.sum((observed_intensities - expected_intensities) ** 2 )
+                chi2_values_good[i, j] = np.sum((observed_intensities - expected_intensities_good) ** 2 )
         # Calculate the 1-sigma and 2-sigma chi-squared thresholds
         chi2_1sigma = 0.064
         chi2_2sigma = 0.256
         # Find the minimum chi-squared value and its position
         min_chi2 = np.min(chi2_values)
         min_pos = np.unravel_index(np.argmin(chi2_values), chi2_values.shape)
+
+        min_chi2_good = np.min(chi2_values_good)
+        min_pos_good = np.unravel_index(np.argmin(chi2_values_good), chi2_values_good.shape)
         
         # Plot the 2D chi-squared profile
         plt.figure(figsize=(8, 6))
@@ -299,13 +284,200 @@ if __name__ == '__main__':
         plt.legend()
         plt.savefig(f"{filepath}/{name_without_extension}_chi2profile.pdf")
         plt.clf()
+        # Plot the 2D chi-squared profile
+        plt.figure(figsize=(8, 6))
+        plt.imshow(chi2_values, origin='lower', extent=[offset_range.min(), offset_range.max(), offset_range.min(), offset_range.max()], cmap='YlGnBu')
+        plt.colorbar(label='$\chi^2$')
         
-#        for po in range(16):
-#        
-#            x_po, y_po = get_coordinates_4x4(po + 1)
-#            
-#            for point in range(1,65):
-#                x_pt, y_pt = get_coordinates_8x8(point)
-#                x_relative, y_relative = convert_coordinates_8x8(x_pt, y_pt, yaml_data)
-#                x_real, y_real = convert_coordinates_4x4(x_po, y_po, x_relative, y_relative, yaml_data, (dx[min_pos], dy[min_pos]))
+        contour1 = plt.contour(dx, dy, chi2_values_good, levels=[chi2_1sigma], colors=['mediumslateblue'])
+        contour2 = plt.contour(dx, dy, chi2_values_good, levels=[chi2_2sigma], colors=['mediumorchid'])
+        plt.plot(0, 0, 'go', label='Original Point')
+        plt.text(-16, -10, '(0, 0)', fontsize=8, color='black', fontweight='bold')
+        plt.plot(dx[min_pos_good], dy[min_pos_good], 'ro', label=f'Minimum $\chi^2$ ({min_chi2_good:.2f})')
+        plt.text(dx[min_pos_good] + 3, dy[min_pos_good] + 3, f'({dx[min_pos_good]: 0.2f}, {dy[min_pos_good]: 0.2f})', fontsize=8, color='firebrick', fontweight='bold')
         
+        # Set the legend labels for the contours
+        plt.clabel(contour1, inline=1, fontsize=10, fmt={chi2_1sigma:r'$\bar{\Delta\mu} = 0.01 $'})
+        plt.clabel(contour2, inline=1, fontsize=10, fmt={chi2_2sigma:r'$\bar{\Delta\mu} = 0.02 $'})
+        
+        plt.xlabel('$\Delta x (mm)$')
+        plt.ylabel('$\Delta y (mm)$')
+        plt.title(f'2D $\chi^2$ Profile of Main-run {run_number} with light-run 127 ')
+        plt.legend()
+        plt.savefig(f"{filepath}/{name_without_extension}_chi2profile_centrallycorrected.pdf")
+        plt.clf()
+
+        light_map_uncorrected = np.empty((16, 16), dtype=float)         
+        light_map_corrected = np.empty((16, 16), dtype=float)         
+        light_map_centrallycorrected = np.empty((16, 16), dtype=float)         
+        for po in range(16):
+        
+            x_po, y_po = get_coordinates_4x4(po + 1)
+            
+            for ch in range(1,17):
+                x_ch, y_ch = get_coordinates_4x4(ch)
+                light_intensity_uncorrected = 0.
+                light_intensity_corrected = 0.
+                light_intensity_centrallycorrected = 0.
+                for pair in convert_1to4(x_ch,y_ch):
+                    x_pt = pair[0]
+                    y_pt = pair[1]
+                    x_real, y_real = convert_coordinates_8x8(x_pt, y_pt, yaml_data)
+                    x_real, y_real = convert_coordinates_4x4(x_po, y_po, x_real, y_real, yaml_data)
+                    x_corr = x_real + dx[min_pos]
+                    y_corr = y_real + dy[min_pos]
+                    x_good = x_real + dx[min_pos_good]
+                    y_good = y_real + dy[min_pos_good]
+             
+                    avr_light_uncorr = average_light_intensity_square(x_real, y_real, 6, rbf)
+                    avr_light_corr = average_light_intensity_square(x_corr, y_corr, 6, rbf)
+                    avr_light_centrallycorr = average_light_intensity_square(x_good, y_good, 6, rbf_good)
+                    light_intensity_uncorrected += avr_light_uncorr
+                    light_intensity_corrected += avr_light_corr
+                    light_intensity_centrallycorrected += avr_light_centrallycorr
+                light_map_uncorrected[po][ch-1] = light_intensity_uncorrected
+                light_map_corrected[po][ch-1] = light_intensity_corrected
+                light_map_centrallycorrected[po][ch-1] = light_intensity_centrallycorrected
+
+        ov_dict = np.empty((16, 16, 6), dtype=float)
+        for po in range(16):
+            hist_vbd = file1.Get(f"vbd/vbd_tile{po}")
+            for ch in range(1, 17):
+                x, y = get_coordinates_4x4(ch)
+                print(po, ch, hist_vbd.GetBinContent(x,y))
+                for ov in range(1, 7):
+                    ov_dict[po][ch-1][ov-1] = ov - hist_vbd.GetBinContent(x,y) 
+        
+        mu_dict = np.empty((16, 16, 6), dtype=float)
+        dcr_dict = np.empty((16, 16, 6), dtype=float)
+        for po in range(16):
+            for ov in range(1, 7):
+                hist_sipm_mu = file1.Get(f"sipm_mu/sipm_mu_ov{ov}_tile{po}")
+                hist_dcr = file1.Get(f"dcr/dcr_ov{ov}_tile{po}")
+                for ch in range(1, 17):
+                    x, y = get_coordinates_4x4(ch)
+                    mu_dict[po][ch - 1][ov-1] = hist_sipm_mu.GetBinContent(x,y)
+                    dcr_dict[po][ch - 1][ov-1] = hist_dcr.GetBinContent(x,y)
+                
+        target_ov = 4.0
+        mu_ov4V_dict = np.empty((16, 16), dtype=float)
+        pde_map_uncorrected = np.empty((16, 16), dtype=float)         
+        pde_map_corrected = np.empty((16, 16), dtype=float)         
+        pde_map_centrallycorrected = np.empty((16, 16), dtype=float)         
+        for position in range(16):
+            for channel in range(16):
+                ov_values = ov_dict[position, channel, :]  # Overvoltage values (0, 1, 2, 3, 4, 5)
+                mu_values = mu_dict[position, channel, :]  # Mu values for the current position and channel
+                dcr_values = dcr_dict[position, channel, :]  # Mu values for the current position and channel
+                
+                # Perform linear interpolation
+                interpolated_mu = np.interp(target_ov, ov_values, mu_values)
+                interpolated_dcr = np.interp(target_ov, ov_values, dcr_values)
+                
+                # Store the interpolated value in mu_ov3V_dict
+                mu_ov4V_dict[position, channel] = interpolated_mu
+                pde_map_uncorrected[position, channel] = interpolated_mu / light_map_uncorrected[position, channel]
+                pde_map_corrected[position, channel] = interpolated_mu / light_map_corrected[position, channel]
+                pde_map_centrallycorrected[position, channel] = interpolated_mu / light_map_centrallycorrected[position, channel]
+                print(position, channel+1, interpolated_dcr)
+
+
+
+        x = np.arange(16)       
+        fig3, ax3 = plt.subplots()
+        ax3.axhline(y=0.44, color='red', linestyle='--', label = 'Min 44%')
+        ax3.axhline(y=0.47, color='blue', linestyle='--', label = 'Typical 47%')
+        for ch in range(16):
+            ax3.scatter(x, pde_map_uncorrected[:, ch], label=f'Channel {ch+1}', s=5)
+            
+              
+        # Set labels and title
+        ax3.set_xlabel('SiPM Tile number')
+        ax3.set_ylabel('PDE')
+        ax3.set_title(f'Uncorrected Photon Detection Efficiencies (Run {run_number})')
+        
+        # Add a horizontal line at y = 0.5
+        #ax2.axhline(y=41.7, color='red', linestyle='--', label = 'Max 41.7')
+        #ax2.axhline(y=13.9, color='blue', linestyle='--', label = 'Typical 13.9')
+        # Add a horizontal line at y = 0.5
+        # Set x-axis ticks and labels
+        ax3.set_xticks(x)
+        ax3.set_xticklabels([str(i) for i in range(16)])
+        
+        # Set grid lines on the y-axis
+        ax3.grid(axis='y')
+        # Set the y-axis limits
+        ax3.set_ylim(0.2, 0.8)
+        # Add a legend
+        #ax.legend()
+        legend3 = ax3.legend(loc='upper right', bbox_to_anchor=(1.4, 1.0))
+        
+        # Adjusting the plot to accommodate the legend
+        plt.subplots_adjust(right=0.7)  # Increase the value to move the legend leftwards
+        plt.savefig(f"{filepath}/pde_tiles_uncorrected_run{run_number}.pdf")
+        plt.clf()
+        fig4, ax4 = plt.subplots()
+        ax4.axhline(y=0.44, color='red', linestyle='--', label = 'Min 44%')
+        ax4.axhline(y=0.47, color='blue', linestyle='--', label = 'Typical 47%')
+        for ch in range(16):
+            ax4.scatter(x, pde_map_corrected[:, ch], label=f'Channel {ch+1}', s=5)
+            
+              
+        # Set labels and title
+        ax4.set_xlabel('SiPM Tile number')
+        ax4.set_ylabel('PDE')
+        ax4.set_title(f'Position-corrected Photon Detection Efficiencies (Run {run_number})')
+        
+        # Add a horizontal line at y = 0.5
+        #ax2.axhline(y=41.7, color='red', linestyle='--', label = 'Max 41.7')
+        #ax2.axhline(y=13.9, color='blue', linestyle='--', label = 'Typical 13.9')
+        # Add a horizontal line at y = 0.5
+        # Set x-axis ticks and labels
+        ax4.set_xticks(x)
+        ax4.set_xticklabels([str(i) for i in range(16)])
+        
+        # Set grid lines on the y-axis
+        ax4.grid(axis='y')
+        # Set the y-axis limits
+        ax4.set_ylim(0.2, 0.8)
+        # Add a legend
+        #ax.legend()
+        legend4 = ax4.legend(loc='upper right', bbox_to_anchor=(1.4, 1.0))
+        
+        # Adjusting the plot to accommodate the legend
+        plt.subplots_adjust(right=0.7)  # Increase the value to move the legend leftwards
+        plt.savefig(f"{filepath}/pde_tiles_corrected_run{run_number}.pdf")
+        plt.clf()
+
+        fig5, ax5 = plt.subplots()
+        ax5.axhline(y=0.44, color='red', linestyle='--', label = 'Min 44%')
+        ax5.axhline(y=0.47, color='blue', linestyle='--', label = 'Typical 47%')
+        for ch in range(16):
+            ax5.scatter(x, pde_map_centrallycorrected[:, ch], label=f'Channel {ch+1}', s=5)
+            
+              
+        # Set labels and title
+        ax5.set_xlabel('SiPM Tile number')
+        ax5.set_ylabel('PDE')
+        ax5.set_title(f'Position-corrected Photon Detection Efficiencies (Run {run_number})')
+        
+        # Add a horizontal line at y = 0.5
+        #ax2.axhline(y=41.7, color='red', linestyle='--', label = 'Max 41.7')
+        #ax2.axhline(y=13.9, color='blue', linestyle='--', label = 'Typical 13.9')
+        # Add a horizontal line at y = 0.5
+        # Set x-axis ticks and labels
+        ax5.set_xticks(x)
+        ax5.set_xticklabels([str(i) for i in range(16)])
+        
+        # Set grid lines on the y-axis
+        ax5.grid(axis='y')
+        # Set the y-axis limits
+        ax5.set_ylim(0.2, 0.8)
+        # Add a legend
+        #ax.legend()
+        legend5 = ax5.legend(loc='upper right', bbox_to_anchor=(1.4, 1.0))
+        
+        # Adjusting the plot to accommodate the legend
+        plt.subplots_adjust(right=0.7)  # Increase the value to move the legend leftwards
+        plt.savefig(f"{filepath}/pde_tiles_centrallycorrected_run{run_number}.pdf")
+        plt.clf()
